5b099e17c31161ce38e4a0592e757fff3a0e7f48
codebox283 <87943295+codebox283@users.noreply.github.com>
2024-04-24 15:00:37 +0530

    Contact and Footer done

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,16 +1,20 @@
 import '../styles/Home.css';
 import Hero from '../assets/Hero.jpg';
 import Ad from '../assets/4.jpg';
-import F2 from '../assets/F2.jpg'
+import F2 from '../assets/F2.jpg';
+import Contact from  '../assets/Contact.jpg';
+import Instagram from '../assets/instagram.svg';
+import LinkedIn from '../assets/linkedin-in.svg';
+import GitHub from "../assets/github.svg";
 
 const Home = () => {
   return (
     <div className='Home'>
       <div className="Navbar">
-        <div className="Logo">
+        <div className="NavLogo">
           <h3>XPENZ<br></br>TRACK</h3>
         </div>
-        <div className='Buttons'>
+        <div className='NavbarButtons'>
           <button>Sign Up</button>
           <button>Log In</button>
         </div>
@@ -80,6 +84,61 @@ const Home = () => {
           <p>XpenZ changed the game for me! Its user-friendly interface and smart features helped me take control of my finances like never before. Thanks to XpenZ, I've redefined my financial success and couldn't be happier!</p>
           <br></br>
           <p>John Doe</p>
+        </div>
+      </div>
+
+      <div className='ContactSection'>
+        <div className='ContactLeft'>
+          <h1>Let's Talk! <br></br> WOOF!</h1>
+          <p>You can ask us questions about our app, and we will be happy to answer you!</p>
+          <button>Contact Us</button>
+        </div>
+        <div className='ContactRight'>
+          <img src={Contact} alt=''></img>
+        </div>
+      </div>
+
+      <div className='Footer'>
+        <div className="FootTop">
+          <div className='FTL'>
+            <h3 className="Logo">XPENZ<br></br>TRACK</h3>
+            <p>Updates right to your inbox</p>
+            <div className='Newsletter'>
+              <input placeholder='Email Address'></input>
+              <button>Send</button>
+            </div>
+          </div>
+          
+          <div className='FTR'>
+            <ul className='Links'>
+              <li>Our Story</li>
+              <li>FAQ</li>
+              <li>Contact</li>
+            </ul>
+            <ul className='Links'>
+              <li>Services</li>
+              <li>Track Money</li>
+              <li>Save Money</li>
+            </ul>
+            <ul className='Links'>
+              <li>Source</li>
+              <li>GitHub</li>
+              <li>Team</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='FootBot'>
+            <span>
+              <p>© 2024 XPENZTRACk</p>
+              <p>Privacy Policy</p>
+              <p>Terms & Conditions</p>
+            </span>
+            <span>
+              <img src={Instagram} alt=''></img>
+              <img src={LinkedIn} alt=''></img>
+              <img src={GitHub} alt=''></img>
+            </span>
         </div>
       </div>
     </div>
