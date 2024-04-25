0780e5fb8fdffaffa103900633e5dc80d0b447e2
Sahil5046 <sahilsk03860@gmail.com>
2024-04-25 22:24:53 +0530

    setup utils folder

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,9 @@
+const asyncHandler = (requestHandle) => {
+    return (req, res, next) => {
+        Promise.resolve(requestHandle(req, res, next)).catch(err => {
+            next(err);
+        })
+    }
+}
+
+export { asyncHandler }
