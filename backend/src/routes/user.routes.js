be75206d0b9d5d3cad439c9eb33c1d577f665de0
Sahil5046 <sahilsk03860@gmail.com>
2024-04-30 12:17:06 +0530

    Use argon2 insted of bcrypt for password cheacking and hashing and fix some issue

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {loginUser, registerUser, logoutUser} from '../controller/user.controller.js'
+import {loginUser, registerUser, logoutUser, updatePassword, forgotPassword} from '../controller/user.controller.js'
 import { verifyJWT } from '../middlewares/auth.middlewares.js'
 const router = Router()
 
@@ -8,5 +8,6 @@ router.route('/register').post(registerUser)
 router.route("/login").post(loginUser)
 
 router.route('/logout').get(verifyJWT, logoutUser)
-
+router.route('/changePassword').patch(verifyJWT, updatePassword)
+router.route('/forgotPassword').patch(forgotPassword)
 export default router
