40162bccb65f6850e00fdae79977f823306a7120
soyeb5047 <soyebsk3861@gmail.com>
2024-08-03 23:15:03 +0530

    add deleteExpense route, controller

--- backend/src/controller/category.controller.js
+++ backend/src/controller/category.controller.js
@@ -14,7 +14,7 @@ const setCategory = asyncHandler(async (req, res) => {
 
     const { amount, name, description } = req.body
 
-    if(!name) throw new ApiError(401, "You must provide a name")
+    if (!name) throw new ApiError(401, "You must provide a name")
 
     if (!categoryArray.includes(name)) throw new ApiError(400, "Invalid category selected")
 
@@ -36,11 +36,45 @@ const setCategory = asyncHandler(async (req, res) => {
     )
 
     return res.status(201)
-    .json(
-        new ApiResponse(201, user, "data saved successfully")
-    )
+        .json(
+            new ApiResponse(201, user, "data saved successfully")
+        )
+})
+
+const deleteExpense = asyncHandler(async (req, res) => {
+    try {
+        const userId = new mongoose.Types.ObjectId(req.cookies?.ID)
+        if (!userId) throw new ApiError(400, "User id is not valid during delete expense")
+
+        const expenseId = new mongoose.Types.ObjectId(req.params.id)
+
+        // check for valid expense id
+        if (!mongoose.Types.ObjectId.isValid(expenseId)) throw new ApiError(400, "invalid expense ID")
+
+        const user = await User.findById(userId)
+
+        // filter from the expense array and set it to user model
+        const expenseArray = user.expenses.filter(expense => !expense.equals(expenseId));
+        user.expenses = expenseArray
+        await user.save()
+
+
+        // console.log(user);
+        
+        await Expense.findOneAndDelete(expenseId)
+
+        return res.status(200).json(
+            new ApiResponse(200, user, "expense deleted successfully")
+        )
+
+
+    } catch (error) {
+
+        console.log("Error while deleting expense: ", error);
+
+        return;
+    }
 })
 
 
-
-export { setCategory }
+export { setCategory, deleteExpense }
--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { setCategory } from "../controller/category.controller.js";
+import { deleteExpense, setCategory } from "../controller/category.controller.js";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
 
 const router = Router()
 
 router.route('/setCategory').post(setCategory, verifyJWT)
+router.route('/delete-expense/:id').get(deleteExpense, verifyJWT)
 
 export default router;
