ad56cdc2b83fc1516f97f58b17c957a7a4801f4a
Sahil5046 <sahilsk03860@gmail.com>
2024-08-03 22:22:57 +0530

    Add Delete Goals controller and set Delete Goal endpoint

--- backend/src/routes/savings.routes.js
+++ backend/src/routes/savings.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { setGoal, EditGoal } from "../controller/savings.controller.js";
+import { setGoal, EditGoal, DeleteGoal } from "../controller/savings.controller.js";
 const router = Router()
 
 router.route('/set-goal').post(setGoal, verifyJWT)
 router.route("/update-goal/:id").patch(EditGoal, verifyJWT);
+router.route('/delete-goal/:id').post(DeleteGoal, verifyJWT);
 
 export default router
